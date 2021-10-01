bb9548cfff5b25f75c42317e9532b4876cd17d0a
ez201205 <ez201205@DESKTOP-GQBCSJ8>
2021-10-01 17:59:02 +0900

    day4

---
+++ java__1/src/Day04/Day04_4배열.java
@@ -0,0 +1,57 @@
+package Day04;
+
+public class Day04_4배열 {
+
+	
+	// 객체 : [클래스] 설계도 기반으로 생성된 메모리 공간
+	// 변수 : [자료형] 하나의 값을 저장할수 있는 메모리 공간
+		// 배열 : 같은 타입의 데이터를 연속된 공간의 나열 , (저장순서) 인덱스 부여	
+			// 같은 타입의 변수의 관리 효율성
+		// 1. 인덱스 : 저장되는 순서번호 [0번부터 시작~ ]
+		// 2. [ ] : [인덱스번호]
+		
+	// int 정수1 , int 정수2 , int 정수3 ===== int 정수 100
+	// int[] 정수 = new int[100]
+	
+	public static void main(String[] args) {
+		
+		// p.151 
+		// 1. 배열 선언 [ 타입[]  배열명 = { 값1 , 값2 , 값3 } ]		
+			// 1. 타입[]
+		
+		
+		
+		
+		int[] scores = { 83 , 90 , 87 };
+			// int[] 배열명 : 배열선언
+				// = ( 값1(인덱스0) , 값2(인덱스1) , 값(인덱스2) )
+		
+		// 2. 배열 호출
+		System.out.println(" scores[0] : "+ scores[0] );
+		System.out.println(" scores[1] : "+ scores[1] );
+		System.out.println(" scores[2] : "+ scores[2] );
+		
+		// 3. 반복문 활용
+		int sum = 0;
+		for( int i = 0; i<3 ; i++ ) { sum += scores[i]; }
+		System.out.println(" 배열내 총합 : " + sum );
+		System.out.println(" 배열내 평균 : " + (sum)/3 );
+		
+		// 예2) // int형 변수 3개를 저장할수 있는 배열 선언
+		int[] 정수배열1 = new int[3]; // => int 4바이트 12칸 생성
+			// 1. 타입 : 자료명/클래스
+				// 2. 배열명 : 아무거나[임의]
+					// 3. new : 메모리할당 연산자
+						// 4. 타입(배열의길이)
+		// 1. 해당 인덱스에 값 넣기
+		정수배열1[0] = 3; 정수배열1[1] = 4; 정수배열1[2] = 5;
+		// 2. 출력
+		System.out.println( 정수배열1 ); // 메모리 주소값 [  첫번째 인덱스 주소 ] 출력
+		System.out.println( 정수배열1[0] ); // 해당 인덱스의 값 출력
+		System.out.println( 정수배열1[1] );
+		
+		
+		
+	}
+	
+}
---
+++ java__1/src/Day04/Day04_7_로또판별기.java
@@ -0,0 +1,95 @@
+package Day04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Day04_7_로또판별기 {
+
+	public static void main(String[] args) {
+		
+		//조건1 1~45사이의 수를 6개 입력받아 배열에저장
+			// 단 중복불가// 1~45사이만 가능
+		// 조건2 추정번호는 난수생성 1~45 사이의 숫자 6개 생성하여 배열에 저장
+		// 조건3 두 배열내 동일한 숫자가 몇개 있는지 체크하여 등수 출력
+		// 출력 몇개가 동일한지 출력하고 등수 출력
+				// 6개 모두 동일하면 1등 // 5개 동일하면 2등 // 4개 동일하면 3등 // 3개 동일하면 4등 // 그외 꽝
+		
+		
+		// 0. 입력객체
+		Scanner scanner = new Scanner(System.in);
+	
+		// 0. 배열 [ 배열선언 : 자료형[] 배열명 = new 자료형[길이] ]
+		int [] number = new int[6]; // int형 6개 변수를 저장할수있는 배열
+		int [] random = new int[6];
+			
+		
+		
+		// 1. 사용자로부터 6개 수를 입역받아 배열에 저장
+		for( int i = 0 ; i<6 ; i++ ) {
+			System.out.println(i+"번째 번호[1~45] 선택 :  ");
+			int num = scanner.nextInt(); 
+			if( num <1 || num > 45 ) { // 0~45 사이가 아닐경우
+				System.out.println(" [[ 알림 ]] : 0~45 사이만 입력가능 합니다 : 다시입력 " );
+				i--;
+				continue;
+			}		
+			boolean check = true; // 중복이 없을 경우는 true 중복이 있을경우는 false
+			// 중복체크 : 만약에 입력한값이 배열내 동일한 값이 존재하면 다시입력
+			for( int j = 0 ; j<6 ; j++) {
+				if( num == number[j] ) { // 중복찾기 성공
+					System.out.println(" [[ 알림 ]] : 선택한 수는 이미 존재 합니다 : 다시 입력 ");
+					i--;
+					check = false;
+					break;
+				}
+			}
+			
+			// 1~45사이 수 이면서 중복수가 아니면 i번째 배열에 저장
+			number[i] = num;
+			
+		}
+		
+		// 2. 출력
+		for( int i = 0 ; i<6 ; i++ ) {
+			System.out.print(number[i] + " ");
+		}
+		System.out.println();
+		
+		// 3. 6개의 난수를 생성해서 배열저장 
+		for( int i = 0 ; i<6 ; i++ ) {
+			Random random2 = new Random();
+			int num = random2.nextInt(45)+1; // 1~45 난수 생성
+			
+			boolean check = true;	// 중복여부 확인 체크
+			for( int j = 0 ; j<6 ; j++ ) {	// 반복하면서 배열내 중복값 찾기
+				if ( num == random[j] ) {	// 만약에 난수가 기존 배열내 값과 동일하면 
+					i--;
+					check = false;
+					break;
+				}
+			}
+			if( check ) random[i] = num;  // 중복값이 없을 경우 i번째에 난수 넣기 
+		}
+		
+		// 4. 출력
+		System.out.print(" [[이번주 추첨 번호]] : ");
+		for( int i = 0 ; i<6 ; i++ ) {
+			System.out.print( random[i] + " ");
+		}
+		System.out.println();
+		
+		int count = 0; // 맞은 수 변수
+		// 5. 두 배열내 동일한 수 체크 
+		for( int i = 0 ; i<6 ; i++ ) { // i는 number 배열의 인덱스
+			for( int j = 0 ; j<6 ; j++ ) { // j는 random 배열의 인덱스 => 36번 비교 [ i 1개당 j는 6번씩 비교 ]
+				if( number[i] == random[j] ) { // 만약에 동일하면 
+					count++; // 맞은 수 변수를 1증가
+				}
+			}
+		}
+		
+		System.out.println(" [[추첨 결과]] : " + count );
+	}
+	
+	
+}
