1c55b4d6ecc67aaf69a13b8fba3506152629fab6
ez201210 <ez201210@DESKTOP-3VGT3L6>
2021-11-02 18:03:27 +0900

    day24

--- java__2/src/controller/Boardviewcontroller.java
+++ java__2/src/controller/Boardviewcontroller.java
@@ -1,14 +1,35 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import domain.Board;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class Boardviewcontroller {
+public class Boardviewcontroller implements Initializable {
 
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Board board = Boardlistcontroller.board;
+		txttitle.setText(board.getB_title());
+		txtcontents.setText(board.getB_contents());
+		if(!Mainpagecontroller.getinstance().getloginid().equals(board.getB_write())) {
+			// 게시물 작성자와 로그인된 작성자가 다를경우
+			btnupdate.setVisible(false);
+			btndelete.setVisible(false);
+			
+		}
+		
+		
+		
+		
+	}
     @FXML
     private Button btnback;
 
